6aa6bc4aa8301d8816630d0f83f5def8655c9ffe
Feerny <luisfpe19@gmail.com>
2022-10-27 07:10:18 -0500

    make it better

--- apiHealthyCo/settings.py
+++ apiHealthyCo/settings.py
@@ -151,7 +151,7 @@ CORS_ORIGIN_WHITELIST=(
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_TMP = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
