a4ac916038d4cbcef08cc2c9c04b46c8721e1fe6
Feerny <luisfpe19@gmail.com>
2022-10-28 07:23:47 -0500

    termine api

--- apiHealthyCo/settings.py
+++ apiHealthyCo/settings.py
@@ -142,6 +142,10 @@ MEDIA_URL = '/media/'
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+MEDIAFILES_DIRS =( 
+os.path.join(BASE_DIR, 'media')
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
