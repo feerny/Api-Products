d9fdb2e018f1a4941cc5afe8bcdda3ddf53e53ec
Feerny <luisfpe19@gmail.com>
2022-11-04 14:18:34 -0500

    agrege nuevos campos

--- healthyapp/urls.py
+++ healthyapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path,include
 from healthyapp import views
 
 urlpatterns = [
-    path('',views.Producto_lista),
     path('api/healthyapp',views.Producto_lista),
     path('api/healthyapp/<int:pk>',views.Producto_detalle),
     path('api/categorias',views.Categoria_lista),
